80644619269b8834f0957bab26b112ba97084f9d
Sid Sun <sid@sidsun.com>
2023-10-02 09:06:07 +0000

    Move backup folder to a subdirectory and replace static path with variables
    It might be possible to do notification restores with its own file
    
    Signed-off-by: Sid Sun <sid@sidsun.com>

--- replicator.sh
+++ replicator.sh
@@ -1,6 +1,9 @@
 #!/usr/bin/dumb-init /bin/bash
 
+LOCAL_PATH=/backup/current
+RESTORE_PATH=/backup/restored
 BACKUP_FILE_NAME=backup.sql
+
 node server/server.js &
 PID=$!
 
@@ -9,21 +12,23 @@ declare -a tables=("monitor" "monitor_tag" "tag" "monitor_group" "group" "status
 
 function dump_tables {
     echo 'dump_tables'
-    # remove previoud backup (if any) and append all tables dumps to one file
-    rm -f /backup/$BACKUP_FILE_NAME
+    # remove previoud backup (if any)
+    rm -rf $LOCAL_PATH
+    mkdir $LOCAL_PATH
+    # append table dumps
     for table in "${tables[@]}"
     do
-        sqlite3 /app/data/kuma.db '.dump "'"$table"'"' >> /backup/$BACKUP_FILE_NAME
+        sqlite3 /app/data/kuma.db '.dump "'"$table"'"' >> $LOCAL_PATH/$BACKUP_FILE_NAME
     done
 }
 
 function debug_output {
-    git diff /backup/$BACKUP_FILE_NAME /backup/restored/backup/$BACKUP_FILE_NAME > /app/data/git-diff.sql
-    diff /backup/$BACKUP_FILE_NAME /backup/restored/backup/$BACKUP_FILE_NAME > /app/data/diff.sql
+    git diff $LOCAL_PATH/$BACKUP_FILE_NAME $RESTORE_PATH/$LOCAL_PATH/$BACKUP_FILE_NAME > /app/data/git-diff.sql
+    diff $LOCAL_PATH/$BACKUP_FILE_NAME $RESTORE_PATH/$LOCAL_PATH/$BACKUP_FILE_NAME > /app/data/diff.sql
     echo "local length" > /app/data/len.txt
-    cat /backup/$BACKUP_FILE_NAME | wc -l >> /app/data/len.txt
+    cat $LOCAL_PATH/$BACKUP_FILE_NAME | wc -l >> /app/data/len.txt
     echo "remote length" >> /app/data/len.txt
-    cat /backup/restored/backup/$BACKUP_FILE_NAME | wc -l >> /app/data/len.txt
+    cat $RESTORE_PATH/$LOCAL_PATH/$BACKUP_FILE_NAME | wc -l >> /app/data/len.txt
 }
 
 function stop_kuma {
@@ -44,10 +49,10 @@ function start_kuma {
 function restic_restore {
     # check MD5s to see if backup is new, restic creates snapshots in cases of no change too
     # see: https://github.com/restic/restic/issues/662
-    restic restore latest --target /backup/restored
-    if test -f "/backup/restored/backup/$BACKUP_FILE_NAME"; then
-        SNAPSHOT_MD5SUM=$(md5sum /backup/restored/backup/$BACKUP_FILE_NAME | awk '{ print $1 }' )
-        LOCAL_MD5SUM=$(md5sum /backup/$BACKUP_FILE_NAME | awk '{ print $1 }' )
+    restic restore latest --target $RESTORE_PATH
+    if test -f "$RESTORE_PATH/$LOCAL_PATH/$BACKUP_FILE_NAME"; then
+        SNAPSHOT_MD5SUM=$(md5sum $RESTORE_PATH/$LOCAL_PATH/$BACKUP_FILE_NAME | awk '{ print $1 }' )
+        LOCAL_MD5SUM=$(md5sum $LOCAL_PATH/$BACKUP_FILE_NAME | awk '{ print $1 }' )
         if [ $LOCAL_MD5SUM != $SNAPSHOT_MD5SUM ]
         then
             echo 'restoring remote backup'
@@ -61,13 +66,13 @@ function restic_restore {
             done
             # Restore DB Data
             # echo 'Restoring data'
-            cat /backup/restored/backup/$BACKUP_FILE_NAME | sqlite3 /app/data/kuma.db # && echo 'tables restored'
+            cat $RESTORE_PATH/$LOCAL_PATH/$BACKUP_FILE_NAME | sqlite3 /app/data/kuma.db # && echo 'tables restored'
             # echo 'Starting services'
             start_kuma
         else
             echo 'remote and local are in sync'
         fi
-        rm -rf /backup/restored
+        rm -rf $RESTORE_PATH
     else
         echo 'backups are not available or cannot be restored'
     fi
@@ -76,22 +81,22 @@ function restic_restore {
 function restic_backup {
     # check MD5s to see if backup is new, restic creates snapshots in cases of no change too
     # see: https://github.com/restic/restic/issues/662
-    restic restore latest --target /backup/restored
-    if test -f "/backup/restored/backup/$BACKUP_FILE_NAME"; then
-        SNAPSHOT_MD5SUM=$(md5sum /backup/restored/backup/$BACKUP_FILE_NAME | awk '{ print $1 }' )
-        LOCAL_MD5SUM=$(md5sum /backup/$BACKUP_FILE_NAME | awk '{ print $1 }' )
+    restic restore latest --target $RESTORE_PATH
+    if test -f "$RESTORE_PATH/$LOCAL_PATH/$BACKUP_FILE_NAME"; then
+        SNAPSHOT_MD5SUM=$(md5sum $RESTORE_PATH/$LOCAL_PATH/$BACKUP_FILE_NAME | awk '{ print $1 }' )
+        LOCAL_MD5SUM=$(md5sum $LOCAL_PATH/$BACKUP_FILE_NAME | awk '{ print $1 }' )
         if [ $LOCAL_MD5SUM != $SNAPSHOT_MD5SUM ]
         then
             echo 'running restic backup'
-            restic backup /backup/$BACKUP_FILE_NAME
+            restic backup $LOCAL_PATH
         else
             echo 'file is the same - skipping backup'
         fi
     else
         echo 'backups are not available or cannot be restored - trying to initiate seed backup anyway'
-        restic backup /backup/$BACKUP_FILE_NAME
+        restic backup $LOCAL_PATH
     fi
-    rm -rf /backup/restored
+    rm -rf $RESTORE_PATH
 }
 
 function restorecon {
